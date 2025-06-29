4b418affd4ac277eabb8c0c4baea7f788aa8951c
Bhargavprasad Vana <bhargavvana80@gmail.com>
2025-06-29 11:29:34 +0530

    Add files via upload

---
+++ Project Files/frontend/src/context/CartContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, quantity = 1) => {
+    setCart(prev => {
+      const existingItem = prev.find(item => item._id === product._id);
+      if (existingItem) {
+        return prev.map(item =>
+          item._id === product._id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        );
+      } else {
+        return [...prev, { ...product, quantity }];
+      }
+    });
+  };
+
+  const removeFromCart = id => {
+    setCart(prev => prev.filter(p => p._id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
