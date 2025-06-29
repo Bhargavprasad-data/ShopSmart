c1b0ed66cf247a610d8cd6a8b40b97606250ba25
Bhargavprasad Vana <bhargavvana80@gmail.com>
2025-06-29 11:03:30 +0530

    Add files via upload

---
+++ Project Files/backend/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      name: String,
+      price: Number,
+      quantity: Number
+    }
+  ],
+  totalPrice: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
